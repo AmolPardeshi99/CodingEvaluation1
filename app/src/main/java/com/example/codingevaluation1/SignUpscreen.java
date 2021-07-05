e7f15742badb76cdb8b103fcb5795e9bc919077e
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-07-05 13:51:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codingevaluation1/SignUpscreen.java
@@ -0,0 +1,14 @@
+package com.example.codingevaluation1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_upscreen);
+    }
+}
